fb434b9ad3f256dfe9fae78fb9a1a0e5c72764d8
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-03-06 00:36:28 -0500

    made a function to randomly select answer for computes, used math.random()

--- script.js
+++ script.js
@@ -11,18 +11,41 @@
 //prompts user for input then sends it to checker
 function askInput(){
     let playerInput = prompt("What do you want to play: Rock, Paper, or Scissors?").toLowerCase();
-    checkInput(playerInput)
+    checkInput(playerInput);
 }
 
 //receives user input from askInput, and checks if it matches the avaiable inputs
 function checkInput(answer){
    if (answer == "rock" || answer == "paper" || answer == "scissors" ){
-    alert("good answer")
+    alert("good answer");
    }
    else{
-    alert("wrong try again")
-    askInput()
+    alert("wrong try again");
+    askInput();
    }
 }
 
-askInput()
+function getComputerChoice(){
+    //math floor picks the number thats between 0 and the max, in this case 3
+    //math random then selects one of those random numbers
+    let rng = Math.floor(Math.random() * 3)
+
+    let comChoice = undefined;
+
+    if (rng == 0){
+        comChoice = "rock"
+    }
+    else if (rng == 1){
+        comChoice = "paper"
+    }
+    else if (rng == 2){
+        comChoice = "scissors"
+    }
+
+    return comChoice
+}
+
+
+
+console.log(getComputerChoice())
+console.log(askInput())
