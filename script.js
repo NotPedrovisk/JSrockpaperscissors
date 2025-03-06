cf5380913b2994e28bcced129c58831dcfb04b14
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-03-06 01:11:59 -0500

    merged askInput and checkInput funcs into one, easier to read and pass values, added function to compare computer and player answers

--- script.js
+++ script.js
@@ -9,21 +9,21 @@
 
 
 //prompts user for input then sends it to checker
+
 function askInput(){
     let playerInput = prompt("What do you want to play: Rock, Paper, or Scissors?").toLowerCase();
-    checkInput(playerInput);
-}
 
-//receives user input from askInput, and checks if it matches the avaiable inputs
-function checkInput(answer){
-   if (answer == "rock" || answer == "paper" || answer == "scissors" ){
-    alert("good answer");
-   }
-   else{
-    alert("wrong try again");
-    askInput();
-   }
-}
+
+    //receives user input from askInput, and checks if it matches the avaiable inputs
+     if (playerInput == "rock" || playerInput == "paper" || playerInput == "scissors" ){
+        return playerInput
+      }
+      else{
+           alert("wrong try again");
+      }
+    
+    }
+
 
 function getComputerChoice(){
     //math floor picks the number thats between 0 and the max, in this case 3
@@ -45,7 +45,31 @@ function getComputerChoice(){
     return comChoice
 }
 
+//checks both answers and then announces who won
+function roundResult(userIn, comIn){
+    //tie checked first, if not a tie continue to next checks
+    if (userIn == comIn){
+        console.log("TIE");
+    }
+    //checks if player won(or wasnt beaten in this case) with any of the 3 options, if didn't win
+    //continue to next check
+    else if (userIn == "rock" && comIn != "paper"){
+        console.log("player won")
+    }
+    else if (userIn == "paper" && comIn != "scissors"){
+        console.log("player won")
+    }
+    else if (userIn == "scissors" && comIn != "rock"){
+        console.log("player won")
+    }
+    //if every other check fails it logically means that computer won
+    else{
+        console.log("Computer won")
+    }
+    console.log(userIn)
+    console.log(comIn)
+}
 
-
-console.log(getComputerChoice())
-console.log(askInput())
+//starts function to check results, it then asks user for input(userIn), and the gets computer's
+//inpute from getComputerChoice for (comIn)
+roundResult(askInput(), getComputerChoice())
